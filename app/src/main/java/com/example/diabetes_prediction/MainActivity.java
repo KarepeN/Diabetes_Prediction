1d52ad78935f65173b3c51c5cbb67eacc5a267af
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-03-05 23:09:36 -0600

    intents
    intents

--- app/src/main/java/com/example/diabetes_prediction/MainActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button predict, medicine;
+    Button predict, medicine,bmi;
+    public static final int SUCCESS=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +23,47 @@ public class MainActivity extends AppCompatActivity {
                 diabetesPrediction(v);
             }
         });
-        medicine=findViewById(R.id.button);
+        medicine=findViewById(R.id.button3);
         medicine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 medicineReminder(v);
             }
         });
+        bmi=findViewById(R.id.button2);
+        bmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmiCalc(v);
+            }
+        });
     }
 
     public void diabetesPrediction(View v){
         Intent explicit = new Intent(this, Prediction.class);
+        startActivityForResult(explicit,SUCCESS );
 
 
     }
 
     public void medicineReminder(View v){
         Intent explicit = new Intent(this, MedicalReminder.class);
+        startActivityForResult(explicit,SUCCESS );
+
+
+    }
+    public void bmiCalc(View v){
+        Intent explicit = new Intent(this, BmiActivity.class);
+        startActivityForResult(explicit,SUCCESS );
 
 
     }
 
-    }
+    public void outAction(View v){
+        Intent implicit = new Intent();
+        implicit.setAction(Intent.ACTION_VIEW);
+        String location = "https://www.healthline.com/health/type-2-diabetes/top-exercises#cycling";
+        Uri data = Uri.parse(location);
+        implicit.setData(data);
+        startActivity(implicit);
+    }    }
