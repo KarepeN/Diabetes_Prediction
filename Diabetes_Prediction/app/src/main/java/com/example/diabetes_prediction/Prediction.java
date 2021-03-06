eea54ce2538b74ab30bc7a24a5aa814f58259e36
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2021-03-05 21:31:21 -0600

    Add files via upload

--- Diabetes_Prediction/app/src/main/java/com/example/diabetes_prediction/Prediction.java
+++ Diabetes_Prediction/app/src/main/java/com/example/diabetes_prediction/Prediction.java
@@ -3,12 +3,21 @@ package com.example.diabetes_prediction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Prediction extends AppCompatActivity {
-
+    private Button button;
+    private EditText txtw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediction);
+        button = (Button) findViewById(R.id.button1);
+        TextView txtvw = (TextView) findViewById(R.id.textView9);
+
+
     }
 }
