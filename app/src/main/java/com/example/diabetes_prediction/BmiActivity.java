fc6a70662b2da4711582738c34cb488b14665007
KarepeN <70028952+KarepeN@users.noreply.github.com>
2021-03-05 23:52:47 -0600

    Update BmiActivity.java

--- app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
@@ -12,6 +12,7 @@ import java.text.DecimalFormat;
 
 public class BmiActivity extends AppCompatActivity {
 
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,24 +27,7 @@ public class BmiActivity extends AppCompatActivity {
     public void computeAction(View v) {
         TextView displayTV = findViewById(R.id.displayTV);
         EditText heightET = findViewById(R.id.heightET);
-        EditText radiusET = findViewById(R.id.weightET);
-        String current = displayTV.getText().toString();
-        try {
-            double height = Double.parseDouble(heightET.getText().toString());
-            double weight = Double.parseDouble(radiusET.getText().toString());
+        EditText weightET = findViewById(R.id.weightET);
 
-
-            if (weight > 0 && height > 0) {
-                double a = weight / Math.pow(height, 2);
-
-                displayTV.setText("BMI " + new DecimalFormat("##.##").format(a));
-            } else {
-
-                displayTV.setText("Enter non-negative values greater than 0");
-            }
-        } catch (NumberFormatException e) {
-            Log.d("Input", "Failed");
-            displayTV.setText("Enter the Values");
-        }
     }
 }
