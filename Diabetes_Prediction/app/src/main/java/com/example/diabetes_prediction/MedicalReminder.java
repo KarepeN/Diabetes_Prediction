fe012850415919ec014dfc6c5e3b8dcc5f5d47f4
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-03-05 19:01:03 -0600

    Add files via upload
    basic files uploaded

---
+++ Diabetes_Prediction/app/src/main/java/com/example/diabetes_prediction/MedicalReminder.java
@@ -0,0 +1,14 @@
+package com.example.diabetes_prediction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_reminder);
+    }
+}
