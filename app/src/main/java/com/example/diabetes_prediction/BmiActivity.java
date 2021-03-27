609f31ccabee95186032dd328119da83a10a6a12
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2021-03-26 22:11:47 -0500

    update BmiActivity.java

--- app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
@@ -2,32 +2,38 @@ package com.example.diabetes_prediction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
 public class BmiActivity extends AppCompatActivity {
 
-  
+    ImageView imgBack;
+    EditText height,weight;
+    Button calculateBtn;
+    TextView bmiTV,tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bmi);
+    }
+        public void calculate(View v) {
 
-        TextView displayTV = findViewById(R.id.displayTV);
-        displayTV.setText("BMI");
+
+
+            tvResult.setText("20.00");
+
+
+        }
 
     }
 
-
-    public void computeAction(View v) {
-        TextView displayTV = findViewById(R.id.displayTV);
-        EditText heightET = findViewById(R.id.heightET);
-        EditText weightET = findViewById(R.id.weightET);
-
-    }
-}
