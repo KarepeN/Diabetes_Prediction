5eb18af5cb29b8bd4eeeab53e68100eec1a09b3c
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2021-03-26 21:03:43 -0500

    update prediction.java

--- app/src/main/java/com/example/diabetes_prediction/Prediction.java
+++ app/src/main/java/com/example/diabetes_prediction/Prediction.java
@@ -2,34 +2,59 @@ package com.example.diabetes_prediction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Prediction extends AppCompatActivity {
-     private Button button;
-    int fCode=1;
+    ImageView imgBack;
+    EditText etGlucose, etBlood, etSkin, etInsulin, etWeight, etHeight, etDiabetesFunc, etAge;
+    Button btnPredict;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediction);
-        button = (Button) findViewById(R.id.button1);
+        etGlucose = findViewById(R.id.etGlucose);
+        etBlood = findViewById(R.id.etBloodP);
+        etSkin = findViewById(R.id.etSkinThick);
+        etInsulin = findViewById(R.id.etInsulin);
+        etWeight = findViewById(R.id.etWeight);
+        etHeight = findViewById(R.id.etHeight);
+        etDiabetesFunc = findViewById(R.id.etDiabetesFunction);
+        etAge = findViewById(R.id.etAge);
+        btnPredict = findViewById(R.id.btnPredict);
+        tvResult = findViewById(R.id.tvResult);
 
-        button = (Button) findViewById(R.id.button);
+
     }
-    public void Pre(View v){
 
-        TextView txtw = findViewById(R.id.textView9);
-        txtw.setText("positive");
+    @SuppressLint("SetTextI18n")
+    private void Predict() {
+
+        int glucose = Integer.parseInt(etGlucose.getText().toString());
+        int blood = Integer.parseInt(etBlood.getText().toString());
+        int skin = Integer.parseInt(etSkin.getText().toString());
+        int insulin = Integer.parseInt(etInsulin.getText().toString());
+        int weight = Integer.parseInt(etWeight.getText().toString());
+        int height = Integer.parseInt(etHeight.getText().toString());
+        int diabetes = Integer.parseInt(etDiabetesFunc.getText().toString());
+        int age = Integer.parseInt(etAge.getText().toString());
+
+
+        tvResult.setText("Result: Diabetes Active");
+
 
     }
-
-    public void onclickcancelBtn(View v){
-        Intent resultint = new Intent(this,MainActivity.class);
-        setResult(fCode,resultint);
-        finish();
-     }
 }
