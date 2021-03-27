37521b996ff1293febf9c00d35c224ca172f1775
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-03-26 20:41:58 -0500

    excercise Activity java file -S540791

---
+++ app/src/main/java/com/example/diabetes_prediction/ExerciseActivity.java
@@ -0,0 +1,41 @@
+package com.example.diabetes_prediction;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ExerciseActivity extends AppCompatActivity {
+ImageView imgback;
+    CardView video1 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+        video1 = findViewById(R.id.video1);
+        
+        imgback  = findViewById(R.id.imgBack);
+        imgback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        video1.setOnClickListener(v -> playVideo("HZH4QASD3ss"));
+       
+
+    }
+
+    private void playVideo(String videoId) {
+        Intent intent = new Intent(ExerciseActivity.this, VideoActivity.class);
+        intent.putExtra("videoid", videoId);
+        startActivity(intent);
+
+    }
+}
