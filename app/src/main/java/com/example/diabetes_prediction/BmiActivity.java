16d1db0e4dddd04ac568daac6fbae15293008089
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2021-03-26 22:47:40 -0500

    update BmiActivity.java

--- app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/BmiActivity.java
@@ -2,7 +2,8 @@ package com.example.diabetes_prediction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,24 +17,43 @@ import android.widget.Toast;
 import java.text.DecimalFormat;
 
 public class BmiActivity extends AppCompatActivity {
-
+int fCode=1;
     ImageView imgBack;
     EditText height,weight;
     Button calculateBtn;
-    TextView bmiTV,tvResult;
+    TextView bmiTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+        imgBack = findViewById(R.id.imgBack);
+        height = findViewById(R.id.etHeight);
+        weight = findViewById(R.id.etWeight);
+        calculateBtn = findViewById(R.id.btnCalculate);
+        bmiTV = findViewById(R.id.tvResult);
+        imgBack.setOnClickListener(v -> onBackPressed());
+        calculateBtn.setOnClickListener(v -> { calculateBMI();
+        });
+
+
+
+
     }
-        public void calculate(View v) {
+    private void calculateBMI()
+    {
+        double weight1 = Double.parseDouble(weight.getText().toString());
+
+        double height1 = Double.parseDouble(height.getText().toString());
+        double result =20.00;
+        bmiTV.setText("your BMI: "+String.format("%.2f", result));
 
 
-
-            tvResult.setText("20.00");
-
-
-        }
 
     }
+    public void onclickcancelBtn(View v){
+        Intent resultint = new Intent(this,MainActivity.class);
+        setResult(fCode,resultint);
+        finish();
+    }
 
+}
