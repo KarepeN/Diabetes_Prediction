fb5239b70ed4e0a24f4cb274920ed790efeb23f4
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-04-04 15:41:31 -0500

    updating 3rd video

--- app/src/main/java/com/example/diabetes_prediction/ExerciseActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/ExerciseActivity.java
@@ -19,6 +19,7 @@ ImageView imgback;
         setContentView(R.layout.activity_exercise);
         video1 = findViewById(R.id.video1);
         video2 = findViewById(R.id.video2);
+        video3 = findViewById(R.id.video3);
 
         imgback  = findViewById(R.id.imgBack);
         imgback.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +31,7 @@ ImageView imgback;
 
         video1.setOnClickListener(v -> playVideo("HZH4QASD3ss"));
         video2.setOnClickListener(v -> playVideo("J7h-5PGbGVc"));
+         video3.setOnClickListener(v -> playVideo("ePylP2XmNRs"));
 
 
     }
