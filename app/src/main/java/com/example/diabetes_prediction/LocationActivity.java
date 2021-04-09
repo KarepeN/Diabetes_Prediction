d7944c9bb7d77d7e9be090fae6a4e81b1206eb87
Sri Vasavi <69984398+Srivasavi-vipparla@users.noreply.github.com>
2021-04-08 22:17:41 -0500

    Update LocationActivity.java

--- app/src/main/java/com/example/diabetes_prediction/LocationActivity.java
+++ app/src/main/java/com/example/diabetes_prediction/LocationActivity.java
@@ -1,83 +1,18 @@
+
 package com.example.diabetes_prediction;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.provider.Settings;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-
-import java.io.IOException;
-import java.net.URI;
-import java.util.List;
-import java.util.Locale;
-
-public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    ImageView imgBack;
-    boolean isPermissionGranted;
-    GoogleMap mgoogleMap;
-    FloatingActionButton floatButton;
-    private FusedLocationProviderClient mLocationClient;
-    private final int GPS_REQUEST_CODE = 9001;
-    EditText etSearch;
-    ImageView btnSearch;
-
-    @Override
+public class LocationActivity extends AppCompatActivity
+{
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-
-        floatButton = findViewById(R.id.floatBtn);
-
-        etSearch = findViewById(R.id.etSearch);
-        btnSearch = findViewById(R.id.btnSearch);
-
-        isGPSEnabled();
-        initMap();
-        mLocationClient = new FusedLocationProviderClient(this);
-        floatButton.setOnClickListener(v -> getCurrentLocation());
-
-        imgBack = findViewById(R.id.imgBack);
-        imgBack.setOnClickListener(v -> finish());
-
-    }
-    public static void hideKeyboard(Activity activity) {
+}
+ public static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
         View view = activity.getCurrentFocus();
@@ -87,64 +22,3 @@ public class LocationActivity extends AppCompatActivity implements OnMapReadyCal
         }
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
-
-
-    @SuppressLint("MissingPermission")
-    private void getCurrentLocation() {
-        mLocationClient.getLastLocation().addOnCompleteListener(task -> {
-            if (task.isSuccessful())
-            {
-                Location location = task.getResult();
-
-
-            }
-        });
-    }
-
-
-
-    private void initMap(){
-        if (isPermissionGranted)
-        {
-
-                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
-                assert supportMapFragment != null;
-                supportMapFragment.getMapAsync(this);
-
-
-
-
-        }
-    }
-    private boolean isGPSEnabled()
-    {
-        LocationManager locationManager  = (LocationManager) getSystemService(LOCATION_SERVICE);
-        boolean provider = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        if (provider)
-        {
-            return true;
-        }
-        else
-        {
-            AlertDialog alertDialog = new AlertDialog.Builder(this).
-                    setTitle("GPS permission")
-                    .setMessage("Gps Required for this app to work")
-                    .setPositiveButton("Yes",((dialog, which) -> {
-                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivityForResult(intent,GPS_REQUEST_CODE);
-                    })).setCancelable(false)
-                    .show();
-
-
-        }
-        return false;
-    }
-
-
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
-}
