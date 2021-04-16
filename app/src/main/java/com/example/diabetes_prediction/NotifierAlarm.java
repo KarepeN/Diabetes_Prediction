8b506942df2a022513e33176c17f5bdd6c7e1902
KarepeN <s540109@nwmissouri.edu>
2021-04-16 17:44:59 -0500

    Create NotifierAlarm.java

---
+++ app/src/main/java/com/example/diabetes_prediction/NotifierAlarm.java
@@ -0,0 +1,63 @@
+package com.example.diabetes_prediction;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Date;
+
+public class NotifierAlarm extends BroadcastReceiver {
+
+    private AppDatabase appDatabase;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        appDatabase = AppDatabase.geAppdatabase(context.getApplicationContext());
+        RoomDAO roomDAO = appDatabase.getRoomDAO();
+        Reminders reminder = new Reminders();
+        reminder.setMessage(intent.getStringExtra("Message"));
+        reminder.setRemindDate(new Date(intent.getStringExtra("RemindDate")));
+        reminder.setId(intent.getIntExtra("id",0));
+        
+        AppDatabase.destroyInstance();
+
+        Uri alarmsound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        Intent intent1 = new Intent(context,MainPage.class);
+        intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.addParentStack(MainPage.class);
+        taskStackBuilder.addNextIntent(intent1);
+
+        PendingIntent intent2 = taskStackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("my_channel_01","hello", NotificationManager.IMPORTANCE_HIGH);
+        }
+
+        Notification notification = builder.setContentTitle("Reminder")
+                .setContentText(intent.getStringExtra("Message")).setAutoCancel(true)
+                .setSound(alarmsound).setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(intent2)
+                .setChannelId("my_channel_01")
+                .build();
+
+
+
+    }
+}
