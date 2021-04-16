bf879a0b308cf3613554ddfe9c43ff6e455fd319
KarepeN <s540109@nwmissouri.edu>
2021-04-16 17:03:08 -0500

    Update RoomDAO.java

--- app/src/main/java/com/example/diabetes_prediction/RoomDAO.java
+++ app/src/main/java/com/example/diabetes_prediction/RoomDAO.java
@@ -13,11 +13,20 @@ import java.util.List;
 @Dao
 public interface RoomDAO {
 
+    @Insert
+    public void Insert(UsernamePassword... usernamePasswords);
+
+    @Update
+    public void Update(UsernamePassword... usernamePasswords);
+
+
+
     @Query("Select * from login where usename = :username")
     public UsernamePassword getUserwithUsername(String username);
 
     @Query("Select * from login where isloggedIn = 1")
     public UsernamePassword getLoggedInUser();
+
 
     @Insert
     public void Insert(Reminders reminders);
@@ -28,5 +37,13 @@ public interface RoomDAO {
     @Delete
     public void Delete(Reminders reminders);
 
+    @Query("Select * from reminder order by remindDate")
+    public List<Reminders> orderThetable();
+
+    @Query("Select * from reminder Limit 1")
+    public Reminders getRecentEnteredData();
+
+    @Query("Select * from reminder")
+    public List<Reminders> getAll();
 
 }
